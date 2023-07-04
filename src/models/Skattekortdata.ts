2aaac7619f25e88ed437c74e27df92c2828a49b6
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-07-04 16:02:17 +0200

    Glemte en trekkode

--- src/models/Skattekortdata.ts
+++ src/models/Skattekortdata.ts
@@ -10,6 +10,7 @@ export type Trekkode =
   | "loennFraHovedarbeidsgiver"
   | "loennFraBiarbeidsgiver"
   | "loennFraNAV"
+  | "pensjonFraNAV"
   | "ufoeretrygdFraNAV"
   | "ufoereytelserFraAndre";
 
