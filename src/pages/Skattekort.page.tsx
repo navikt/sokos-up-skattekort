c91f6f53d3b94ce3be113c24ea1bb3f8a05252bf
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-08-08 13:14:58 +0200

    Bruker Zod, refaktor visning av skattekort

--- src/pages/Skattekort.page.tsx
+++ src/pages/Skattekort.page.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SkattekortSearch from "../components/SkattekortSearch";
 import Skattekortvisning from "../components/Skattekortvisning";
 import useSWRImmutable from "swr/immutable";
 import { skattekortDataUrl } from "../api/urls";
 import { fetcher } from "../api/api";
-import SkattekortData from "../models/SkattekortData";
+import SkattekortData, { SkattekortSchema } from "../models/SkattekortData";
 import { Alert, Heading, Loader } from "@navikt/ds-react";
 import styles from "./Skattekort.module.css";
+import { isEmpty } from "../util/utils";
 
 type SkattekortPersonRequestBody = {
   fnr: string;
@@ -15,7 +16,7 @@ type SkattekortPersonRequestBody = {
 
 const SkattekortPage = () => {
   const [searchBody, setSearchBody] = useState<SkattekortPersonRequestBody>();
-
+  const [skattekortData, setSkattekortData] = useState<SkattekortData>();
   const query = searchBody
     ? {
         path: skattekortDataUrl,
@@ -29,14 +30,43 @@ const SkattekortPage = () => {
 
   const { data, isLoading, error: fetchError } = useSWRImmutable<SkattekortData>(query, fetcher);
 
+  useEffect(() => {
+    if (data) {
+      const safeParse = SkattekortSchema.safeParse(data);
+
+      if (!safeParse.success) {
+        console.error("Ugyldig skattekortdataresultat" + safeParse.error.message);
+      } else {
+        setSkattekortData(safeParse.data);
+      }
+    } else setSkattekortData(undefined);
+  }, [data]);
+
   const handleSubmit = (fnr: string, year: number) => {
     const searchParameters = { fnr, inntektsaar: year };
     if (fnr && year) setSearchBody(() => searchParameters);
     else setSearchBody(undefined);
   };
 
-  const showSkattekort = data && !isLoading && !fetchError && !!data.skattekortListe?.length;
-  const emptySkattekort = data && !isLoading && !fetchError && !data.skattekortListe?.length;
+  const showSkatteKort = () => {
+    let jsx;
+    if (isLoading)
+      jsx = (
+        <div className={styles.skattekort__loader}>
+          <Loader size="3xlarge" title="Henter Skattekort" />
+        </div>
+      );
+    else if (fetchError) jsx = <Alert variant="error">En feil oppstod, prøv igjen</Alert>;
+    else if (!skattekortData) jsx = <></>;
+    else if (isEmpty(skattekortData.skattekortListe))
+      jsx = (
+        <Alert variant="warning">
+          {`Ingen skattekort funnet for år ${searchBody?.inntektsaar} med bruker ${searchBody?.fnr}`}
+        </Alert>
+      );
+    else jsx = <Skattekortvisning data={skattekortData} />;
+    return jsx;
+  };
 
   return (
     <div className={styles.skattekort}>
@@ -44,20 +74,8 @@ const SkattekortPage = () => {
         Skattekort
       </Heading>
       <SkattekortSearch handleSubmit={handleSubmit} />
-      {showSkattekort && <Skattekortvisning data={data} />}
-      {isLoading && (
-        <div className={styles.skattekort__loader}>
-          <Loader size="3xlarge" title="Henter Skattekort" />
-        </div>
-      )}
-      {emptySkattekort && (
-        <Alert variant="warning">
-          {`Ingen skattekort funnet for år ${searchBody?.inntektsaar} med bruker ${searchBody?.fnr}`}
-        </Alert>
-      )}
-      {fetchError && <Alert variant="error">En feil oppstod, prøv igjen</Alert>}
+      {showSkatteKort()}
     </div>
   );
 };
-
 export default SkattekortPage;
