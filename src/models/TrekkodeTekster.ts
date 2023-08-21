6bb5a9e50b667e02dbd5a750c0609599d211045e
mudasar187 <mudasar_cheema@hotmail.com>
2023-08-21 14:57:23 +0200

    legger til flere trekkode tekster

--- src/models/TrekkodeTekster.ts
+++ src/models/TrekkodeTekster.ts
@@ -4,7 +4,15 @@ export const TrekkodeTekster = new Map<Trekkode, string>([
   ["loennFraHovedarbeidsgiver", "Lønn fra hovedarbeidsgiver"],
   ["loennFraBiarbeidsgiver", "Lønn fra biarbeidsgiver"],
   ["loennFraNAV", "Lønn fra NAV"],
+  ["pensjon", "Pensjon"],
   ["pensjonFraNAV", "Pensjon fra NAV"],
+  ["loennTilUtenrikstjenestemann", "Lønn til utenrikstjenestemann"],
+  ["loennKunTrygdeavgiftTilUtenlandskBorger", "Lønn kun trygdeavgift til utenlandsk borger"],
+  [
+    "loennKunTrygdeavgiftTilUtenlandskBorgerSomGrensegjenger",
+    "Lønn kun trygdeavgift til utenlandsk borger som grensegjenger",
+  ],
   ["ufoeretrygdFraNAV", "Uføretrygd fra NAV"],
   ["ufoereytelserFraAndre", "Uføreytelser fra andre"],
+  ["introduksjonsstoenad", "Introduksjonsstønad"],
 ] as const);
