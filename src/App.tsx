1130e290e171a32229f6e35165e7fa514de26490
mudasar187 <mudasar_cheema@hotmail.com>
2023-08-22 14:27:52 +0200

    legger til grafana faro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.module.css";
 import SkattekortPage from "./pages/Skattekort.page";
+import { useEffect } from "react";
+import { initGrafanaFaro } from "./util/grafanaFaro";
 
 const App = () => {
+  useEffect(() => {
+    initGrafanaFaro();
+  }, []);
+
   return <SkattekortPage />;
 };
 
---
+++ src/util/grafanaFaro.ts
@@ -0,0 +1,29 @@
+import { getEnvironment } from "./environment";
+import { initializeFaro } from "@grafana/faro-web-sdk";
+
+type TelemetryCollectorURL =
+  | "https://telemetry.nav.no/collect"
+  | "https://telemetry.ekstern.dev.nav.no/collect"
+  | "http://localhost:12347";
+
+const getTelemetryCollectorURL = (): TelemetryCollectorURL => {
+  if (getEnvironment() === "production") {
+    return "https://telemetry.nav.no/collect";
+  }
+
+  if (getEnvironment() === "development") {
+    return "https://telemetry.ekstern.dev.nav.no/collect";
+  }
+
+  return "http://localhost:12347";
+};
+
+export function initGrafanaFaro() {
+  initializeFaro({
+    isolate: true,
+    url: getTelemetryCollectorURL(),
+    app: {
+      name: "sokos-op-skattekort",
+    },
+  });
+}
