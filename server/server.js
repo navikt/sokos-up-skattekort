9af6c0c90f9b1916397b59cf9cfdb7aed8edc9d3
mudasar187 <mudasar_cheema@hotmail.com>
2023-12-13 13:09:26 +0100

    gjort navnendring og oppdatert avhengigheter

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const basePath = "/sokos-op-skattekort";
+const basePath = "/sokos-up-skattekort";
 const buildPath = path.resolve(__dirname, "../dist");
 const server = express();
 const corsAllowedOrigin = process.env.CORS_ALLOWED_ORIGIN || "http://localhost:5173";
