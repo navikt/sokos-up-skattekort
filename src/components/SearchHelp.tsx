d9b87f2c2ae5549032e12414355b2c269b71011d
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-07-08 09:45:50 +0200

    Flytter infotext

--- src/components/SearchHelp.tsx
+++ src/components/SearchHelp.tsx
@@ -1,16 +1,13 @@
-import { Alert } from "@navikt/ds-react";
+import { HelpText } from "@navikt/ds-react";
 
 const SearchHelp = () => (
-  <Alert variant="info">
-    <p>
-      <strong>Informasjon om Skattekort:</strong>
-    </p>
-    <p>
-      Skattekort viser skattekortet slik det er mottatt fra Skatteetaten. For å se hvilken del av skattekortet som vil
-      bli, eller er, brukt i en beregning må menypunktet "Skatt og trekk" og undermenypunkt "eSkattekort - Søk" i
-      Økonomiportal benyttes.
-    </p>
-  </Alert>
+  <HelpText title="Informasjon om Skattekort" placement="left-start">
+    <strong>Informasjon om Skattekort:</strong>
+    <br />
+    Skattekort viser skattekortet slik det er mottatt fra Skatteetaten. For å se hvilken del av skattekortet som vil
+    bli, eller er, brukt i en beregning må menypunktet "Skatt og trekk" og undermenypunkt "eSkattekort - Søk" i
+    Økonomiportal benyttes.
+  </HelpText>
 );
 
 export default SearchHelp;
