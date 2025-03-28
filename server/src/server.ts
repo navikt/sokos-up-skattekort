855648241ef76ac3564278e91661bb93460fef22
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-03-28 16:22:57 +0100

    Oppdater /server og bruk Typescript

---
+++ server/src/server.ts
@@ -0,0 +1,35 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+import expressStaticGzip from "express-static-gzip";
+import path from "path";
+
+const basePath = "/sokos-up-skattekort";
+const buildPath = path.resolve(__dirname, "../dist");
+const server = express();
+
+const corsAllowedOrigin = [
+  /https:\/\/utbetalingsportalen(-[a-z][a-z0-9])?(.ansatt|.intern)(.dev)?.nav.no/,
+  "https://sokos-utbetalingsportalen-astro.intern.dev.nav.no",
+  "http://localhost:5173",
+];
+
+server.use(cors({ origin: corsAllowedOrigin }));
+
+server.use(
+  basePath,
+  expressStaticGzip(buildPath, {
+    enableBrotli: true,
+    orderPreference: ["br"],
+  }),
+);
+
+server.get(`${basePath}/internal/isAlive`, (_req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+server.get(`${basePath}/internal/isReady`, (_req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+// eslint-disable-next-line no-console
+server.listen(8080, () => console.log("Server listening on port 8080"));
